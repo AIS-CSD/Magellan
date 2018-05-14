b8d355f36c94f3aa22d62ac7eaa8409973b997d6
AIS-CSD <39259225+AIS-CSD@users.noreply.github.com>
2018-05-14 15:07:07 +0700

    Add files via upload

---
+++ examples/POST_MAGELLAN/POST_MAGELLAN.ino
@@ -0,0 +1,24 @@
+#include "Magellan.h"
+Magellan magel;
+char auth[]="4e4371e0-d030-11e7-ab12-3bdd0ed7935b"; 		//Token Key you can get from magellan platform
+
+String payload;
+
+void setup() {
+  Serial.begin(9600);
+  magel.begin(auth);           //init Magellan LIB
+}
+
+void loop() {
+
+  /*
+  	Example send random temperature and humidity to Magellan IoT platform
+  */
+  String Temperature=String(random(0,100));
+  String Humidity=String(random(0,100));
+
+  payload="{\"Temperature\":"+Temperature+",\"Humidity\":"+Humidity+"}";       //please provide payload with json format
+
+  magel.post(payload);                            							   //post payload data to Magellan IoT platform
+  
+}
