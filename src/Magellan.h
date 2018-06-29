fc020b69b6aade2827cfb87279d31de1ad49ba3d
AIS-CSD <39259225+AIS-CSD@users.noreply.github.com>
2018-06-29 13:50:27 +0700

    Update Magellan.h

--- src/Magellan.h
+++ src/Magellan.h
@@ -1,19 +1,25 @@
 #ifndef Magellan_h
 #define Magellan_h
 
-#define ATLSOFTSERIAL 1
+
 #include <Arduino.h>
 #include <Stream.h>
+
 
 #define MODE_STRING 0
 #define MODE_STRING_HEX 1
 
-/*
-#if ATLSOFTSERIAL 
-	#include "AltSoftSerial.h"
-#endif
-	#include <SoftwareSerial.h>
-*/
+
+#if defined( __ARDUINO_X86__)
+    #define ATLSOFTSERIAL 0
+#elif defined(ARDUINO_NUCLEO_L476RG)
+    #define ATLSOFTSERIAL 0
+#elif defined(ARDUINO_AVR_UNO) || (ARDUINO_AVR_MEGA2560)
+	#define ATLSOFTSERIAL 1
+#else 
+    #define ATLSOFTSERIAL 1
+#endif 
+
 #if ATLSOFTSERIAL 
 	#include "AltSoftSerial.h"
 #endif
@@ -21,43 +27,21 @@
   const byte maxretrans=4;	
 
   const char con[]="40";
+  const char contk[]="42";
   const char non_con[]="50";
   const char ack[]="60";
+  const char acktk[]="62";
   const char rst[]="70";
 
-  const char EMPTY[]="00";
+  //const char EMPTY[]="00";
   const char GET[]="01";
   const char POST[]="02";
   const char PUT[]="03";
   const char DELETE[]="04";
 
-struct AIS_BC95_RES
-{
-	char status;
-	String data;
-	String temp;
-};
-
-/*
-struct signal
-{
-	String csq;
-	String rssi;
-	String ber;
-};
-*/
-
-/*
-struct pingRESP
-{
-	bool status;
-	String addr;
-	String ttl;
-	String rtt;
-};
-*/
 
 enum rspcode {
+	EMPTY=00,
 	CREATED=65,
 	DELETED=66,
 	VALID=67,
@@ -90,37 +74,106 @@ public:
 	Magellan();
 
 	bool debug;
-	bool default_server;
+	//bool default_server;
 	bool printstate=true;
-
-	//void reset();
-	//bool getNBConnect();
-	//signal getSignal();
-    //pingRESP pingIP(char *IP);
 
     //################# Magellan Platform ########################
     bool begin(char *auth);
-    void printHEX(char *str);
-    void printmsgID(unsigned int messageID);
-    void cnt_auth_len(char *auth);
     String post(String payload);
-    void send(String payload,int style=1,int interval=20);
-    String get(String Resource,String Report="");
-    String get_data(String Resource);
-    void  waitResponse();
-private:
-	
-	AIS_BC95_RES  wait_rx_bc(long tout,String str_wait);
-	void print(char *str);
-	void print_rsp_header(const String Msgstr);
-	void print_rsp_Type(const String Msgstr,unsigned int msgID);
-	void miniresponse(String rx);
+    String get(String Resource);
+    String get_dashboard(String Resource);
+    String get_api(String Resource,String Proxy);
+    String linenotify(String token,String msg);
 
+private:
+
+	char *pathauth;
+	unsigned int Msg_ID=0;
+	//################### Buffer #######################
+	String data_input;
+	String data_buffer;
+	String send_buffer;
+	String rcvdata="";
+	//################### counter value ################
+	byte k=0;
+	//################## flag ##########################
+	bool breboot_flag=false;
+	bool end=false;
+	bool send_NSOMI=false;
+	bool flag_rcv=true;
+	bool en_get=true;
+	bool en_post=true;
+	bool en_send=true;
+	bool en_chk_buff=false;
+	bool getpayload=false;
+	bool Created=false;
+	bool sendget=false;
+	bool NOTFOUND=false;
+	bool GETCONTENT=false;
+	bool RCVRSP=false;
+	bool success=false;
+	bool connected=false;
+	bool get_process=false;
+	bool post_process=false;
+	bool ACK=false;
+	bool EMP=false;
+	bool send_ACK=false;
+	bool more_flag=false;
+	bool hw_connected=false;
+	//################### Parameter ####################
+	//unsigned int resp_cnt=0;
+	unsigned int resp_msgID=0;
+	unsigned int post_ID=0;
+	unsigned int post_token=0;
+	unsigned int get_ID=0;
+	unsigned int get_token=0;
+	unsigned int auth_len=0;
+	unsigned int path_hex_len=0;
+	//-------------------- timer ----------------------
+	unsigned int previous_send=0;
+	unsigned int previous_check=0;
+	unsigned long previous=0;
+	unsigned int wait_time=5;
+	unsigned int previous_time_get=0;
+	//unsigned int last_msgid=0;
+	unsigned int token=0;
+	unsigned int rsptoken=0;
+	//################### Diagnostic & Report ###########
+	byte maxtry=0;
+	byte cnt_retrans=0;
+	byte cnt_retrans_get=0;
+	byte cnt_error=0;
+	byte cnt_cmdgetrsp=0;
+	byte cnt_rcv_resp=0;
+	String LastError="";
+	//###################################################
+
+	//################# Function #######################
+	void reboot_module();
+	void setup_module();
+	void check_module_ready();
+	void _serial_flush();
+    	void printHEX(char *str);
+    	void printmsgID(unsigned int messageID);
+    	void cnt_auth_len(char *auth);
+	void print(char *str);
+	void print_rsp_header(String Msgstr);
+	void print_rsp_Type(String Msgstr,unsigned int msgID);
+	unsigned int option_len(unsigned int path_len);
+	void print_uripath(String uripath);
+
+	void miniresponse(String rx);
+	String post_data(String payload,String option1,String option2,String option3);
+	String get_data(String Resource,String Proxy);
+	void Msgsend(String payload,String option1,String option2,String option3);
+	void Msgget(String Resource,String Proxy);
 	void print_pathlen(unsigned int path_len,char *init_str);
+	void  waitResponse();
 
 protected:
 	 Stream *_Serial;
 };
 
+extern Magellan nbiot;
 
 #endif
